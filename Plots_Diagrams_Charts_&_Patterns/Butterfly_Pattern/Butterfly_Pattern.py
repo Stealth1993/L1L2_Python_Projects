2d341733ea4f952b1faf3e4255a45044c45cd3ac
Stealth <jha.santosh.1216@gmail.com>
2025-04-25 18:25:03 +0530

    Butterfly_Pattern

---
+++ Plots_Diagrams_Charts_&_Patterns/Butterfly_Pattern/Butterfly_Pattern.py
@@ -0,0 +1,16 @@
+import numpy as np
+import matplotlib.pyplot as plt
+
+t = np.linspace(0, 2 * np.pi, 360)
+x = np.sin(t) * (np.exp(np.cos(t)) - 2 * np.cos(4 * t))
+y = np.cos(t) * (np.exp(np.cos(t)) - 2 * np.cos(4 * t))
+
+plt.figure(figsize=(8, 8))
+
+plt.plot(x, y, color='purple', linewidth=2)
+plt.plot(-x, y, color='purple', linewidth=2)
+
+plt.title('Butterfly Pattern', fontsize=20, fontweight='bold')
+plt.axis('equal')
+plt.axis('off')
+plt.show()
