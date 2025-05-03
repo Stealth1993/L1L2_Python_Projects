54fe2278e8cafdcd9240e0dab9e7bd69d46c4cc5
Stealth <jha.santosh.1216@gmail.com>
2025-05-03 05:43:51 +0530

    Network_Graph

--- Plots_Diagrams_Charts_&_Patterns/Network_Graph/Network_Graph.py
+++ Plots_Diagrams_Charts_&_Patterns/Network_Graph/Network_Graph.py
@@ -6,7 +6,7 @@ G = nx.Graph()
 G.add_nodes_from(["A", "B", "C", "D", "E"])
 G.add_edges_from([("A", "B"), ("A", "C"), ("B", "D"), ("C", "D"), ("D", "E")])
 
-pos = nx.spring_layout(G, seed=92)  # positions for all nodes
+pos = nx.spring_layout(G)  # positions for all nodes
 
 nx.draw_networkx_nodes(G, pos, node_size=700, node_color='lightblue')
 nx.draw_networkx_edges(G, pos, width=2, alpha=0.5, edge_color='gray')   
