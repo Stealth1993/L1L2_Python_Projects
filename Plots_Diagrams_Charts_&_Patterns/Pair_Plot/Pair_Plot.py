c5f8505781c24d41386f1cd3f21b74dfc3fe3240
Stealth <jha.santosh.1216@gmail.com>
2025-05-05 10:52:01 +0530

    Pair_Plot

--- Plots_Diagrams_Charts_&_Patterns/Pair_Plot/Pair_Plot.py
+++ Plots_Diagrams_Charts_&_Patterns/Pair_Plot/Pair_Plot.py
@@ -4,6 +4,7 @@ import pandas as pd
 import numpy as np
 
 df = sns.load_dataset("iris")
-sns.pairplot(df, hue="species", palette="Set2")
+sns.pairplot(df, hue="species", palette="Set1", diag_kind="kde",
+                 markers=["o", "s", "D"], plot_kws={"alpha": 0.7, "s": 50})
 plt.suptitle("Pair Plot of Iris Dataset", y=1.02)
 plt.show()
