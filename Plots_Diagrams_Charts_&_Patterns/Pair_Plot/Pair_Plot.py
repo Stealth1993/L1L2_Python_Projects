0a97cebab4a01496ef063539a18a644299a85523
Stealth <jha.santosh.1216@gmail.com>
2025-05-05 10:53:27 +0530

    Pair_Plot

--- Plots_Diagrams_Charts_&_Patterns/Pair_Plot/Pair_Plot.py
+++ Plots_Diagrams_Charts_&_Patterns/Pair_Plot/Pair_Plot.py
@@ -1,7 +1,5 @@
 import matplotlib.pyplot as plt
 import seaborn as sns
-import pandas as pd
-import numpy as np
 
 df = sns.load_dataset("iris")
 sns.pairplot(df, hue="species", palette="Set1", diag_kind="kde",
