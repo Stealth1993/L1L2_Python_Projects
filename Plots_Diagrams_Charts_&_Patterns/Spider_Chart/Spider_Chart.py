479bdf7ef0b95f17106fc82d34fa2ba42b48c110
Stealth <jha.santosh.1216@gmail.com>
2025-05-08 00:00:45 +0530

    Spider_Chart

--- Plots_Diagrams_Charts_&_Patterns/Spider_Chart/Spider_Chart.py
+++ Plots_Diagrams_Charts_&_Patterns/Spider_Chart/Spider_Chart.py
@@ -1,31 +1,20 @@
 import matplotlib.pyplot as plt
 import numpy as np
 
-def spider_chart(data, categories, title='Spider Chart', save_path=None):
-    # Number of variables
-    num_vars = len(categories)
+categories = ['Speed', 'Reliability', 'Comfort', 'Safety', 'Efficiency']
+values = [4, 3, 5, 2, 4]
 
-    # Compute angle for each axis
-    angles = np.linspace(0, 2 * np.pi, num_vars, endpoint=False).tolist()
+# Number of variables
+N = len(categories)
+angle = np.linspace(0, 2 * np.pi, N, endpoint=False).tolist()
+values += values[:1]  # Repeat the first value to close the circle
+angle += angle[:1]  # Repeat the first angle to close the circle
 
-    # The plot is a circle, so we need to "complete the loop" and append the start to the end.
-    data = np.concatenate((data, [data[0]]))
-    angles += angles[:1]
-
-    # Create the spider chart
-    fig, ax = plt.subplots(figsize=(6, 6), subplot_kw=dict(polar=True))
-    ax.fill(angles, data, color='blue', alpha=0.25)
-    ax.plot(angles, data, color='blue', linewidth=2)
-
-    # Labels for each axis
-    plt.xticks(angles[:-1], categories)
-
-    # Title
-    plt.title(title)
-
-    # Save or show the plot
-    if save_path:
-        plt.savefig(save_path)
-        print(f"Spider chart saved to {save_path}")
-    else:
-        plt.show()
+fig, ax = plt.subplots(figsize=(6, 6), subplot_kw=dict(polar=True))
+ax.fill(angle, values, color='red', alpha=0.25)
+ax.set_yticklabels([])  # Hide the radial ticks
+ax.set_xticks(angle[:-1])  # Set the angular ticks
+ax.set_xticklabels(categories)  # Set the category labels
+ax.set_title('Spider Chart Example', size=20, color='blue', weight='bold')
+ax.grid(True)  # Show the grid
+plt.show()
