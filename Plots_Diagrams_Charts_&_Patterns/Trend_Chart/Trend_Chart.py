ab0d3a70b48650225fb1110810a6a2a7cd9fb26a
Stealth <jha.santosh.1216@gmail.com>
2025-05-15 19:33:45 +0530

    Trend_Chart

--- Plots_Diagrams_Charts_&_Patterns/Trend_Chart/Trend_Chart.py
+++ Plots_Diagrams_Charts_&_Patterns/Trend_Chart/Trend_Chart.py
@@ -3,7 +3,7 @@ import seaborn as sns
 import pandas as pd
 import numpy as np
 
-years = ['2014', '2015', '2016', '2017', '2018', '2019', '2020', '2021', '2022']
+years = ['2017', '2018', '2019', '2020', '2021', '2022']
 
 languages = ['Python', 'Java', 'C++', 'JavaScript', 'Ruby', 'PHP']
 
