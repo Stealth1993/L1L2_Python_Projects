cbf4fd3cf6c1cf88e866d4be9ca39a2d1919b951
Stealth <jha.santosh.1216@gmail.com>
2025-06-07 20:32:46 +0530

    Dirac_Delta_Approximation_Plot

---
+++ Python_for_STEM/Dirac_Delta_Approximation_Plot/Dirac_Delta_Approximation_Plot.py
@@ -0,0 +1,28 @@
+import numpy as np
+import matplotlib.pyplot as plt
+
+# parameters
+N = 1000  # number of points
+x = np.linspace(-5, 5, N)  # x values
+sigma = 0.1  # standard deviation for Gaussian approximation
+
+# Gaussian function as an approximation of the Dirac delta function
+def gaussian(x, sigma):
+    return (1 / (sigma * np.sqrt(2 * np.pi))) * np.exp(-0.5 * (x / sigma) ** 2)
+
+# Calculate the Gaussian approximation
+y = gaussian(x, sigma)
+
+# Plotting the Gaussian approximation
+plt.figure(figsize=(10, 6))
+plt.plot(x, y, label=f'Gaussian Approximation (σ={sigma})', color='blue')
+plt.title('Dirac Delta Function Approximation using Gaussian Function')
+plt.xlabel('x')
+plt.ylabel('Amplitude')
+plt.axhline(0, color='black', lw=0.5, ls='--')
+plt.axvline(0, color='black', lw=0.5, ls='--')
+plt.xlim(-5, 5)
+plt.ylim(-0.1, 1.5)
+plt.grid()
+plt.legend()
+plt.show()
