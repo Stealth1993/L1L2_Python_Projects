7910bc57b16c14c276ced15c6059786dc8b203d9
Stealth <jha.santosh.1216@gmail.com>
2025-06-27 23:05:51 +0530

    Synchrotron_Radiation_with_Electron_Distribution

--- Python_for_STEM/Synchrotron_Radiation_with_Electron_Distribution/Synchrotron_Radiation_with_Electron_Distribution.py
+++ Python_for_STEM/Synchrotron_Radiation_with_Electron_Distribution/Synchrotron_Radiation_with_Electron_Distribution.py
@@ -2,29 +2,30 @@ import numpy as np
 import matplotlib.pyplot as plt
 from scipy.special import iv
 
-# Parameters 
-B = 1e-6 # Magnetic field
-nu = np.logspace(8, 12, 100) # Frequency (Hz)
-p = 2.5 # Power-low index
-N0 = 1e5 # Normalization
+# Description: This program computes synchrotron radiation from a power-law electron distribution,
+# integrating over the electron energy spectrum.
+
+# Parameters
+B = 1e-6  # Magnetic field (T)
+nu = np.logspace(8, 12, 100)  # Frequency (Hz)
+p = 2.5  # Power-law index
+N0 = 1e5  # Normalization
 
 # Synchrotron critical frequency and emissivity
 def synchrotron_emissivity(nu, B, p):
     nu_c = 4.2e6 * B * (nu / 1e9)**(2 / (p + 1))
     x = nu / nu_c
-    F = x**(1/3) * np.exp(-x)
-    return N0 * B**((p +1 ) / 2) * F
+    F = x**(1/3) * np.exp(-x)  # Approximation of synchrotron function
+    return N0 * B**((p + 1) / 2) * F
 
 # Spectrum
 P_nu = synchrotron_emissivity(nu, B, p)
 
 # Plot
 plt.figure(figsize=(10, 6))
-plt.plot(nu, P_nu)
-plt.xscale('log')
-plt.yscale('log')
+plt.loglog(nu, P_nu)
+plt.title('Synchrotron Radiation from Power-Law Electron Distribution')
 plt.xlabel('Frequency (Hz)')
-plt.ylabel('Spectral Power Density (W/Hz)')
-plt.title('Synchrotron Radiation Spectrum')
-plt.grid()
+plt.ylabel('Power (arbitrary units)')
+plt.grid(True)
 plt.show()
